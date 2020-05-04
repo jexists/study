a2813cd0d63a48754afc0f983816145a4d1a62b4
jexists <jexists486@gmail.com>
2020-05-04 16:26:40 +0900

    router 연결

--- d_angular/200501_inflearn/front-end/src/app/section/clock/clock.component.ts
+++ d_angular/200501_inflearn/front-end/src/app/section/clock/clock.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import * as moment from 'moment/moment';
+import { Observable, interval } from 'rxjs';
+import { map, distinctUntilChanged } from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-clock',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
 
-  constructor() { }
+  timeString: string;
+
+  constructor(
+    private router: Router,
+  ) { 
+    // setInterval(()=>{
+    // },1000)
+    
+    // rxJs 사용
+    interval(1000)
+      .pipe(map(() => {
+        return moment().format('YYYY-MM-DD  HH:mm:ss')
+      }))
+      .subscribe(data => {
+        this.timeString = data;
+      })
+  }
 
   ngOnInit(): void {
   }
 
+  // https://momentjs.com/
+  //install => yarn add moment
+  //yard add rxjs
+  getCurrentTime(): string {
+     let result = moment().format('YYYY-MM-DD HH:mm:ss')
+     return result;
+  }
+
+
+
+
+  goStopwatch(): void {
+    this.router.navigateByUrl('/stopwatch');
+  }
 }
--- d_angular/200501_inflearn/front-end/src/app/section/stopwatch/stopwatch.component.ts
+++ d_angular/200501_inflearn/front-end/src/app/section/stopwatch/stopwatch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stopwatch',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class StopwatchComponent implements OnInit {
 
   present: string ='20';
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
@@ -19,5 +22,8 @@ export class StopwatchComponent implements OnInit {
     
   }
 
+  goClock(): void {
+    this.router.navigateByUrl('/clock');
+  }
 
 }
