91c5d86d3f6b482ac62bee21f2909a97cd113d17
jexists <jexists486@gmail.com>
2020-05-12 18:42:43 +0900

    앱 네비게이션 추가하기

--- d_angular/200511_heroes/heroes-app/src/app/core/hero.service.ts
+++ d_angular/200511_heroes/heroes-app/src/app/core/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Observable, of, from } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -22,5 +22,10 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- d_angular/200511_heroes/heroes-app/src/app/heroes/heroes.component.ts
+++ d_angular/200511_heroes/heroes-app/src/app/heroes/heroes.component.ts
@@ -20,21 +20,21 @@ export class HeroesComponent implements OnInit {
 
   // heroes = HEROES;
   heroes: Hero[];
-  selectedHero: Hero;
+  // selectedHero: Hero;
 
   constructor(
     private heroService: HeroService,
-    private messageSevice: MessageService
+    // private messageSevice: MessageService
   ) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageSevice.add(`HeroService: Seleected hero id = ${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageSevice.add(`HeroService: Seleected hero id = ${hero.id}`);
+  // }
 
   // getHeroes(): void {
   //   this.heroes = this.heroService.getHeroes();
