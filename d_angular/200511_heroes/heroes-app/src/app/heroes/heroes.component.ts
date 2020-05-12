7749ca540b64a1ead67ba745afaa408ed30ac821
jexists <jexists486@gmail.com>
2020-05-12 17:55:07 +0900

    서비스 추가하기

--- d_angular/200511_heroes/heroes-app/src/app/heroes/heroes.component.ts
+++ d_angular/200511_heroes/heroes-app/src/app/heroes/heroes.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../core/hero';
-import { HEROES } from '../core/mock-heroes';
+// import { HEROES } from '../core/mock-heroes';
+
+import { HeroService } from '../core/hero.service';
+import { MessageService } from '../core/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,14 +18,29 @@ export class HeroesComponent implements OnInit {
   //   name: 'Windstorm'
   // };
 
-  heroes = HEROES;
-  constructor() { }
+  // heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
+  constructor(
+    private heroService: HeroService,
+    private messageSevice: MessageService
+  ) { }
+
   ngOnInit(): void {
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageSevice.add(`HeroService: Seleected hero id = ${hero.id}`);
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
