3fb23114a4d0e66e7316a1fe37ddc561be64eba7
jexists <jexists486@gmail.com>
2020-06-30 20:41:32 +0900

    movie app start

--- e_react/200701_movie_app/src/App.js
+++ e_react/200701_movie_app/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+//app.js
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000)
+  }
+  render(){
+    const { isLoading } = this.state;
+    return (
+      <div>{isLoading ? "로딩중..." : "로딩완료"}</div>
+    )
+  }
 }
 
 export default App;
