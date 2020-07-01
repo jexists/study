dffd0bf04418cac5b9e3b1927861aaeffb2ec099
jexists <jexists486@gmail.com>
2020-07-01 14:12:22 +0900

    genres add

--- e_react/200701_movie_app/src/Movie.js
+++ e_react/200701_movie_app/src/Movie.js
@@ -3,14 +3,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="movie_genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres">{genre}</li>
+            ))}
+        </ul>
+        <p className="movie_summary">{summary}</p>
       </div>
     </div>
   );
@@ -22,7 +27,11 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
+
+Movie.defaultProps = {
+  genres:[]
+}
 
 export default Movie;
