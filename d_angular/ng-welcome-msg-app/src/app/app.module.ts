5966b1b6b33e933c6360c8d52a5c65321645cad9
jexists <jexists486@gmail.com>
2020-07-29 17:45:16 +0900

    code chapter 1

--- d_angular/ng-welcome-msg-app/src/app/app.module.ts
+++ d_angular/ng-welcome-msg-app/src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, COMPOSITION_BUFFER_MODE } from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 import { LangSelectorComponent } from './lang-selector/lang-selector.component';
 import { I18nSupportService } from './i18n-support.service';
+import { LangSelectorBtnPipe } from './lang-selector/lang-selector-btn.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeMsgComponent,
-    LangSelectorComponent
+    LangSelectorComponent,
+    LangSelectorBtnPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    // HttpModule,
   ],
   providers: [
     {provide: COMPOSITION_BUFFER_MODE, useValue: false},
