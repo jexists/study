2be200bf322b5af8cc97eb177c1b24ca1e2fc79c
jexists <jexists486@gmail.com>
2020-07-29 16:53:14 +0900

    ngmodel

--- d_angular/ng-welcome-msg-app/src/app/welcome-msg/welcome-msg.component.ts
+++ d_angular/ng-welcome-msg-app/src/app/welcome-msg/welcome-msg.component.ts
@@ -5,28 +5,15 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './welcome-msg.component.html',
   styleUrls: ['./welcome-msg.component.scss']
 })
-export class WelcomeMsgComponent implements AfterViewInit {
+export class WelcomeMsgComponent {
 
   constructor() { }
 
   userName = "";
   private valid = false;
-  private static Check_Keyup_Wait_Sec = 5000;
 
-  ngAfterViewInit() {
-    const checkTouchedFn = () => {
-      if (this.valid) return;
-      console.log('이름입력');
-    };
-    setTimeout(checkTouchedFn, WelcomeMsgComponent.Check_Keyup_Wait_Sec);
-  }
-
-  onKeyUp(name) {
-    this.valid = name.length > 0;
-  }
-
-  setName(name) {
-    this.userName = name;
+  onChange() {
+    this.valid = this.userName.length > 0;
   }
 
 
