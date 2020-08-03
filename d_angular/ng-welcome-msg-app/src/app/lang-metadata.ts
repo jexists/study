2c8a96b6016ae7253c861159a06c13df4504d713
jexists <jexists486@gmail.com>
2020-08-03 10:41:13 +0900

    flag add

--- d_angular/ng-welcome-msg-app/src/app/lang-metadata.ts
+++ d_angular/ng-welcome-msg-app/src/app/lang-metadata.ts
@@ -2,19 +2,19 @@ export const LANG_METADATA = [
   {
     code: 'ko',
     name: '한국어',
-    flagImgName: '',
+    flagImgName: 'korea.png',
     msg: '안녕하세요'
   },
   {
     code: 'en',
     name: '영어',
-    flagImgName: '',
+    flagImgName: 'usa.png',
     msg: 'Hello'
   },
   {
     code: 'fi',
     name: '따갈로그',
-    flagImgName: '',
+    flagImgName: 'philippine.png',
     msg: 'Magandang Umaga'
   },
 ]
