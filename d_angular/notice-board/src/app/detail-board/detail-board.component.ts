64cb17c51ee5fa823bd51a73eb896732f725dd20
jexists <jexists486@gmail.com>
2020-08-10 12:08:54 +0900

    notice board main

---
+++ d_angular/notice-board/src/app/detail-board/detail-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-board',
+  templateUrl: './detail-board.component.html',
+  styleUrls: ['./detail-board.component.scss']
+})
+export class DetailBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
