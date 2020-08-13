e751232790fe57a9c3eb7a6b81657eaa860e99d5
jexists <jexists486@gmail.com>
2020-08-13 14:30:44 +0900

    map data

--- y_test/200807-kakao-map/kakao-map/src/app/app.module.ts
+++ y_test/200807-kakao-map/kakao-map/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { MapInfoComponent } from './map-info/map-info.component';
 import { Map2Component } from './map2/map2.component';
+import { SearchMapComponent } from './search-map/search-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     MapInfoComponent,
-    Map2Component
+    Map2Component,
+    SearchMapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- y_test/200807-kakao-map/kakao-map/src/app/search-map/search-map.component.ts
+++ y_test/200807-kakao-map/kakao-map/src/app/search-map/search-map.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from '../service/map.service';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 declare let kakao: any;
 
 @Component({
-  selector: 'app-map2',
-  templateUrl: './map2.component.html',
-  styleUrls: ['./map2.component.scss']
+  selector: 'app-search-map',
+  templateUrl: './search-map.component.html',
+  styleUrls: ['./search-map.component.scss']
 })
-export class Map2Component implements OnInit {
+export class SearchMapComponent implements OnInit {
+
   map: any;
   myLatLng: any;
   marker: any;
+  place: any;
+
+  searchField: FormGroup;
 
   // MARKERINFO======================
   markerInfo = [
@@ -53,7 +57,16 @@ export class Map2Component implements OnInit {
   ngOnInit(): void {
     //지도 실행
     this.getMap();
-
+    this.onFormGroupInit();
+  }
+  onFormGroupInit(): void {
+    this.searchField = new FormGroup({
+      keyword: new FormControl(null, Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(100)
+      ])),
+    });
   }
 
   getMap() {
@@ -79,22 +92,53 @@ export class Map2Component implements OnInit {
       //확대 축소 컨트롤 위치 (자동디자인)
       map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
+      // 장소 검색 객체를 생성합니다
+      this.place = new kakao.maps.services.Places();
+
+
+      // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
       //마커 글씨 생성
       this.onMakeMarker();
-      // this.onTestMarker();
     });
   }
 
+  searchPlaces(keyword) {
+    // console.log(keyword);
+    var keyword = keyword;
 
-  // forEACH 사용
+    if (!keyword.replace(/^\s+|\s+$/g, '')) {
+      alert('키워드를 입력해주세요!');
+      return false;
+    }
 
-  onTestMarker() {
-
+    // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+    this.place.keywordSearch(keyword, this.placesSearchCB);
   }
 
+  placesSearchCB(data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+      console.log(data);
+      
+      // 정상적 검색이 완료 검색 목록과 마커를 표출
+      // this.displayPlaces(data);
 
+      // 페이지 번호를 표출합니다
+      // displayPagination(pagination);
+      console.log('페이지 번호 표출');
+    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+      alert('검색 결과가 존재하지 않습니다.');
+      return;
+    } else if (status === kakao.maps.services.Status.ERROR) {
+      alert('검색 결과 중 오류가 발생했습니다.');
+      return;
+    }
+    
+  }
 
-
+  displayPlaces(data) {
+    console.log(data);
+  }
 
   onMakeMarker(): void {
     var overlay = [];
@@ -109,17 +153,13 @@ export class Map2Component implements OnInit {
         image: new kakao.maps.MarkerImage(this.markerInfo[i].imgSrc, this.markerInfo[i].markerSz) // 마커 이미지 
       });
 
-      // console.log(marker[i]);
       overlay[i] = new kakao.maps.CustomOverlay({
         content: this.markerInfo[i].content,
         map: this.map,
         position: this.markerInfo[i].latlng
       });
-      
       overlay[i].setMap(null);
-      var selectedMarker = null;
-      var selectedInfo = null;
-      
+
       // 마커에 mouseover 이벤트를 등록
       kakao.maps.event.addListener(marker[i], 'mouseover', function () {
         // 클릭된 마커가 없고, mouseover된 마커가 클릭된 마커가 아니면
@@ -127,7 +167,7 @@ export class Map2Component implements OnInit {
           overlay[i].setMap(marker[i].f);
         }
       });
-      
+
       // 마커에 mouseout 이벤트를 등록합니다
       kakao.maps.event.addListener(marker[i], 'mouseout', function () {
         // 클릭된 마커가 없고, mouseout된 마커가 클릭된 마커가 아니면
@@ -135,11 +175,10 @@ export class Map2Component implements OnInit {
           overlay[i].setMap(null);
         }
       });
-      
-      // 마커에 click 이벤트를 등록합니다
+      var selectedMarker = null;
+      var selectedInfo = null;
+      // 마커에 click 이벤트
       kakao.maps.event.addListener(marker[i], 'click', function () {
-        
-        // 클릭된 마커가 없고, click 마커가 클릭된 마커가 아니면
         if (!selectedMarker || selectedMarker !== marker[i]) {
           overlay[i].setMap(marker[i].f);
           !!selectedInfo && selectedInfo.setMap(null);
@@ -149,4 +188,26 @@ export class Map2Component implements OnInit {
       });
     }
   } //for문
+
+
+  // 현재위치 버튼 클릭하면 현재위치 이동
+  onMyPlace() {
+    var points = [
+      this.myLatLng, //현재위치 (다른위치로 하고싶은경우 아래 코드사용)
+      // new kakao.maps.LatLng(33.452278, 126.567803),
+    ];
+
+    // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성
+    var bounds = new kakao.maps.LatLngBounds();
+
+    // 배열의 좌표들이 잘 보이게 마커를 지도에 추가
+    for (let i = 0; i < points.length; i++) {
+      // let marker = new kakao.maps.Marker({ position: points[i] });
+      // marker.setMap(this.map);
+      // LatLngBounds 객체에 좌표를 추가
+      bounds.extend(points[i]);
+    }
+    //지도범위 재설정 (중심좌표, 레벨 바뀜)
+    this.map.setBounds(bounds);
+  }
 }
