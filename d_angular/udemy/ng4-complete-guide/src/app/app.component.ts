28f0a168c5670ade56f3e57bbdd5f3c87ec36e04
jexists <jexists486@gmail.com>
2020-08-22 22:17:27 +0900

    input output

--- d_angular/udemy/ng4-complete-guide/src/app/app.component.ts
+++ d_angular/udemy/ng4-complete-guide/src/app/app.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  serverElements = [];
+  serverElements = [
+    {
+      type: 'server',
+      name: 'Testserver',
+      content: 'Just a test!'
+    }
+  ];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'bluePrint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
