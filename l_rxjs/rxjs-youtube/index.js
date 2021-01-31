d234ed5c7b58ceef41ceeb0c842c329ae3657879
jexists <jexists486@gmail.com>
2021-01-31 19:32:43 +0900

    js vs rxjs compare

--- l_rxjs/rxjs-youtube/index.js
+++ l_rxjs/rxjs-youtube/index.js
@@ -2,6 +2,33 @@
 
 const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-  console.log('click');
-});
+// button.addEventListener('click', () => {
+//   console.log('click');
+// });
+
+// rxjs.fromEvent(button, 'click').subscribe(() => {
+//   return console.log('click');
+// });
+
+// let count = 0;
+// button.addEventListener('click', () => {
+//   console.log(('click', ++count));
+// });
+
+// rxjs.fromEvent(button, 'click')
+//   .pipe(
+//     rxjs.operators.scan((count) => count + 1, 0)
+//   ).subscribe((count) => {
+//     return console.log('click', count);
+//   });
+
+
+rxjs
+  .fromEvent(button, 'click')
+  .pipe(
+    rxjs.operators.throttleTime(1000),
+    rxjs.operators.scan((count) => count + 1, 0)
+  ).subscribe((count) => {
+    return console.log('click', count);
+  });
+
