c23d4e12a607b82b6db6727695f03e59d4c74753
jexists <jexists486@gmail.com>
2021-02-06 15:11:30 +0900

    observable basic

--- l_rxjs/rxjs-setup/src/index.ts
+++ l_rxjs/rxjs-setup/src/index.ts
@@ -23,12 +23,65 @@
 //     console.log('error' + err);
 //   },
 //   complete() {
-//     console.log('done'); 
+//     console.log('done');
+//   }
+// }
+
+// observable.subscribe(observer);
+
+// Create Observable
+// import { Observable } from "rxjs";
+
+// const observable = new Observable(function subscribe(subscriber) {
+//   const id = setInterval(() => {
+//     subscriber.next("hi");
+//   }, 1000);
+// });
+
+//subscribing to observables
+
+// observable.subscribe((x:string) => console.log(x));
+// observable.subscribe((x:string) => console.log(x + '!'));
+
+// Executing observables (discussed)
+
+
+// import { Observable } from 'rxjs';
+
+// const observable = new Observable((observer) => {
+//   try {
+//     observer.next(1);
+//     observer.next(2);
+//     observer.next(3);
+//     observer.next(4);
+//     observer.complete();
+//   } catch(err) {
+//     observer.error(err);
+//   }
+//   setTimeout(()=>{
+//   },1000)
+// });
+
+// const observer = {
+//   next(x: number) {
+//     console.log('got value ' + x);
+//   },
+//   error(err: string) {
+//     console.log('error' + err);
+//   },
+//   complete() {
+//     console.log('done');
 //   }
 // }
 
 // observable.subscribe(observer);
 
 
-// Create Observable
-import { Observable } from 'rxjs';
+// disposing Observable Executions
+
+
+import { from, Observable, of } from 'rxjs';
+
+const observable = of([10, 20, 30])
+const subscription = observable.subscribe((x)=>console.log(x));
+subscription.unsubscribe();
