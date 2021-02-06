96a7bef198e70ddce3c5b2ed3bd71bdf3193c1bf
jexists <jexists486@gmail.com>
2021-02-06 11:43:54 +0900

    observable

--- l_rxjs/rxjs-setup/src/index.ts
+++ l_rxjs/rxjs-setup/src/index.ts
@@ -1,1 +1,34 @@
-console.log('rxjs');
+// console.log('rxjs');
+
+// import { Observable } from 'rxjs';
+
+// const observable = new Observable((observer) => {
+//   //observer
+//   observer.next(1);
+//   observer.next(2);
+//   observer.next(3);
+//   // observer.complete();
+//   setTimeout(()=>{
+//     observer.next(4);
+//     observer.complete();
+//     observer.next(5); //X
+//   },1000)
+// });
+
+// const observer = {
+//   next(x: number) {
+//     console.log('got value ' + x);
+//   },
+//   error(err: string) {
+//     console.log('error' + err);
+//   },
+//   complete() {
+//     console.log('done'); 
+//   }
+// }
+
+// observable.subscribe(observer);
+
+
+// Create Observable
+import { Observable } from 'rxjs';
