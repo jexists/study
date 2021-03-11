0f2d4f9d12230f7d37f739f3527044eb3678faf8
jexists <jexists486@gmail.com>
2021-03-12 08:27:32 +0900

    py data type study

---
+++ p_python/python_practice/string_type.py
@@ -0,0 +1,32 @@
+
+# 작은 따옴표
+print('jexists') #jexists
+
+# 큰 따옴표
+print("jexists") #jexists
+
+# 개행문자를 이용한 줄바꿈
+print('Hello\nPython!') 
+#Hello
+#Python!
+
+# 문자열 덧셈
+print('Hello' + 'WORKS!') #HelloWORKS!
+
+# 문자열 곱셈
+print('Hello' * 3) #HelloHelloHello
+
+# len함수로 문자열의 길이 계산
+print(len('jexists')) #7
+
+print('hello'[0]) #h
+
+print('hello'[1]) #e
+
+print('hello'[4]) #o
+
+print('hello'[-1]) #o
+
+print('hello'[-2]) #l
+
+print('hello'[0:4]) #hell
