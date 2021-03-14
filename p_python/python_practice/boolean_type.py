6ba3d938892f3ba851469bb848e50938243b04e8
jexists <jexists486@gmail.com>
2021-03-14 16:34:26 +0900

    data type

---
+++ p_python/python_practice/boolean_type.py
@@ -0,0 +1,21 @@
+
+print(3 == 3) #True
+
+print(3 != 3) #False
+
+print(3 < 3) #False
+
+print(3 > 3) #False
+
+print(3 <= 3) #True
+
+print(3 >=3) #True
+
+a = True
+b = False
+
+print(a and b) #False
+
+print(a or b) #True
+
+print(not a) #False
