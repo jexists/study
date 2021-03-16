ac59e6d2bf403a301f5d5c9b85eac5f41fe92664
jexists <jexists486@gmail.com>
2021-03-16 22:23:07 +0900

    for statement

---
+++ p_python/python_practice/for_statement.py
@@ -0,0 +1,36 @@
+
+basket = ['apple', 'mango', 'cherry']
+
+# list 반복문
+for fruit in basket:
+    print(f'저는 {fruit}을 가지고 있습니다.')
+    print('반복 예스!')  # 들여쓰기해서 반복문에 속함
+print('반복 노노노')
+# 저는 apple을 가지고 있습니다.
+# 반복 예스!
+# 저는 mango을 가지고 있습니다.
+# 반복 예스!
+# 저는 cherry을 가지고 있습니다.
+# 반복 예스!
+# 반복 노노노
+
+# string 반복문
+for alphabet in 'jexists':
+    print(alphabet)
+# j
+# e
+# x
+# i
+# s
+# t
+# s
+
+# 숫자 반복문
+# range(5) = range(0이상, 5미만) = range(0, 5)단축키
+for number in range(5):
+    print(number)
+# 0
+# 1
+# 2
+# 3
+# 4
