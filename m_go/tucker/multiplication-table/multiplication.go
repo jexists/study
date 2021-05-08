2be51bee2edf302ed7861b078759610846f0e8a9
jexists <jexists486@gmail.com>
2021-05-08 23:25:16 +0900

    3x2빼고 출력

--- m_go/tucker/multiplication-table/multiplication.go
+++ m_go/tucker/multiplication-table/multiplication.go
@@ -6,11 +6,14 @@ func main() {
 
 	for num := 2; num < 10; num++ {
 		fmt.Printf("%d단 \n", num)
-		if num == 5 {
-			continue
-		}
+		// if num == 5 {
+		// 	continue
+		// }
 
 		for num2 := 1; num2 < 10; num2++ {
+			if num == 3 && num2 == 2 {
+				continue
+			}
 			fmt.Println(num, num2, num*num2)
 		}
 	}
