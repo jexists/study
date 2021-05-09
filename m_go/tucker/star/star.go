c2083a18ace8de980c2f505c1e53572562baefad
jexists <jexists486@gmail.com>
2021-05-09 19:49:22 +0900

    for example -star

---
+++ m_go/tucker/star/star.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	for i := 0; i < 5; i++ {
+// 		for j := 0; j < i+1; j++ {
+// 			fmt.Print("*") //줄바꿈없음
+// 			// fmt.Println('*') // +\n 자동줄바꿈
+// 		}
+// 		fmt.Println()
+// 	}
+// }
+// *
+// **
+// ***
+// ****
+// *****
+
+// func main() {
+// 	for i := 0; i < 5; i++ {
+// 		for j := 0; j < 5-i; j++ {
+// 			fmt.Print("*") //줄바꿈없음
+// 			// fmt.Println('*') // +\n 자동줄바꿈
+// 		}
+// 		fmt.Println()
+// 	}
+// }
+// *****
+// ****
+// ***
+// **
+// *
+
+// func main() {
+// 	for i := 0; i < 3; i++ {
+// 		for j := 0; j < i+1; j++ {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println("")
+// 	}
+
+// 	for i := 0; i < 3; i++ {
+// 		for j := 2; j > i; j-- {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println("")
+// 	}
+// }
+//*
+//**
+//***
+//**
+//*
+
+// func main() {
+// 	for i := 0; i < 4; i++ {
+// 		for j := 4; j > i; j-- {
+// 			fmt.Print(" ")
+// 		}
+// 		for k := 0; k < 2*i+1; k++ {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println("")
+// 	}
+// }
+// *
+// ***
+// *****
+// *******
+
+func main() {
+	for i := 0; i < 4; i++ {
+		for j := 4; j > i+1; j-- {
+			fmt.Print(" ")
+		}
+		for k := 0; k < 2*i+1; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	for i := 0; i < 4; i++ {
+		for l := 0; l < i+1; l++ {
+			fmt.Print(" ")
+		}
+		for m := 6; m > 2*i+1; m-- {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+
+// 	*
+// ***
+// *****
+// *******
+// *****
+// ***
+//  *
