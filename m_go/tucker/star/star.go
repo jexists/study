1822da960b66cb9d2c90c444a2e01c1b9b28b17b
jexists <jexists486@gmail.com>
2021-05-09 20:18:51 +0900

    answer

--- m_go/tucker/star/star.go
+++ m_go/tucker/star/star.go
@@ -54,6 +54,28 @@ import "fmt"
 //*
 
 // func main() {
+// 	for i := 0; i < 3; i++ {
+// 		for j := 0; j < i+1; j++ {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println("")
+// 	}
+
+// 	for i := 0; i < 2; i++ {
+// 		for j := 0; j < 2-i; j++ {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println("")
+// 	}
+// }
+
+// *
+// **
+// ***
+// **
+// *
+
+// func main() {
 // 	for i := 0; i < 4; i++ {
 // 		for j := 4; j > i; j-- {
 // 			fmt.Print(" ")
@@ -64,27 +86,73 @@ import "fmt"
 // 		fmt.Println("")
 // 	}
 // }
-// *
+//  *
 // ***
 // *****
 // *******
 
+// func main() {
+// 	for i := 0; i < 4; i++ {
+// 		for j := 0; j < 3-i; j++ {
+// 			fmt.Print(" ")
+// 		}
+// 		for j := 0; j < i*2+1; j++ {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println("")
+// 	}
+// }
+
+//  *
+// ***
+// *****
+// *******
+
+// func main() {
+// 	for i := 0; i < 4; i++ {
+// 		for j := 4; j > i+1; j-- {
+// 			fmt.Print(" ")
+// 		}
+// 		for k := 0; k < 2*i+1; k++ {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println()
+// 	}
+
+// 	for i := 0; i < 4; i++ {
+// 		for l := 0; l < i+1; l++ {
+// 			fmt.Print(" ")
+// 		}
+// 		for m := 6; m > 2*i+1; m-- {
+// 			fmt.Print("*")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
+// 	*
+// ***
+// *****
+// *******
+// *****
+// ***
+//  *
+
 func main() {
-	for i := 0; i < 4; i++ {
-		for j := 4; j > i+1; j-- {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 2-i; j++ {
 			fmt.Print(" ")
 		}
-		for k := 0; k < 2*i+1; k++ {
+		for j := 0; j < 2*i+1; j++ {
 			fmt.Print("*")
 		}
 		fmt.Println()
 	}
 
-	for i := 0; i < 4; i++ {
-		for l := 0; l < i+1; l++ {
+	for i := 0; i < 2; i++ {
+		for j := 0; j < i+1; j++ {
 			fmt.Print(" ")
 		}
-		for m := 6; m > 2*i+1; m-- {
+		for j := 0; j < 3-i*2; j++ {
 			fmt.Print("*")
 		}
 		fmt.Println()
