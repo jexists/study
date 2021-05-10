975de824792c55f0c5f261b657072ad06a43485f
jexists <jexists486@gmail.com>
2021-05-10 21:58:47 +0900

    재귀호출 설명

--- m_go/tucker/function/func.go
+++ m_go/tucker/function/func.go
@@ -13,19 +13,50 @@ import "fmt"
 // }
 
 func main() {
-	a, b := fun1(2, 3)
-	fmt.Println(a, b) //3 2
+	// a, b := fun1(2, 3)
+	// fmt.Println(a, b) //3 2
+
+	f1(10)
 }
 
 // func fun1(x, y int) (int, int) {
 // 	return y, x
 // }
 
-func fun1(x, y int) (int, int) {
-	fun2(x, y)
-	return y, x
-}
+// func fun1(x, y int) (int, int) {
+// 	fun2(x, y)
+// 	return y, x
+// }
 
-func fun2(x, y int) {
-	fmt.Println("func2", x, y) //func2 2 3
+// func fun2(x, y int) {
+// 	fmt.Println("func2", x, y) //func2 2 3
+// }
+
+func f1(x int) {
+	if x == 0 {
+		return
+	}
+	fmt.Printf("f1(%d) before call f1(%d)\n", x, x-1)
+	f1(x - 1)
+	fmt.Printf("f1(%d) after call f1(%d)\n", x, x-1)
+	// f1(10) before call f1(9)
+	// f1(9) before call f1(8)
+	// f1(8) before call f1(7)
+	// f1(7) before call f1(6)
+	// f1(6) before call f1(5)
+	// f1(5) before call f1(4)
+	// f1(4) before call f1(3)
+	// f1(3) before call f1(2)
+	// f1(2) before call f1(1)
+	// f1(1) before call f1(0)
+	// f1(1) after call f1(0)
+	// f1(2) after call f1(1)
+	// f1(3) after call f1(2)
+	// f1(4) after call f1(3)
+	// f1(5) after call f1(4)
+	// f1(6) after call f1(5)
+	// f1(7) after call f1(6)
+	// f1(8) after call f1(7)
+	// f1(9) after call f1(8)
+	// f1(10) after call f1(9)
 }
