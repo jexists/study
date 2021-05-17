f8ea8c107e4032301f735c1daa1f39b73b1ab851
jexists <jexists486@gmail.com>
2021-05-17 21:23:41 +0900

    struct

---
+++ m_go/tucker/struct/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	var p Person                       //초깃값 { 0}
+	p1 := Person{"sadness", 17}        //{sadness 17}
+	p2 := Person{name: "joy", age: 21} //{joy 21}
+	p3 := Person{name: "anger"}        //{anger 0}
+	p4 := Person{}                     //{ 0}
+
+	fmt.Println(p, p1, p2, p3, p4) //{ 0} {sadness 17} {joy 21} {anger 0} { 0}
+
+	p.name = "happy"
+	p.age = 27
+
+	fmt.Println(p, p1, p2, p3, p4) //{happy 27} {sadness 17} {joy 21} {anger 0} { 0}
+}
