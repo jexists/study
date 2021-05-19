7d8321bb607bcd723243118d1a0441a6ea33c175
jexists <jexists486@gmail.com>
2021-05-19 19:30:34 +0900

    struct

--- m_go/tucker/struct/student.go
+++ m_go/tucker/struct/student.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// struct 속성(statement)/타입
 type Student struct {
 	name  string
 	class int
@@ -14,12 +15,24 @@ type Grade struct {
 	grade string
 }
 
+//Student struct가 가지고 있는 기능 (객체에 속한 매소드)
 func (s Student) ViewGrade() {
 	fmt.Println(s.grade)
 }
 
+func (s Student) InputGrade(name string, grade string) {
+	s.grade.name = name
+	s.grade.grade = grade
+}
+
+//일반함수
 func ViewGrade(s Student) {
 	fmt.Println(s.grade)
+}
+
+func InputGrade(s Student, name string, grade string) {
+	s.grade.name = name
+	s.grade.grade = grade
 }
 
 func main() {
@@ -33,4 +46,7 @@ func main() {
 
 	s.ViewGrade() //{Math C}
 	ViewGrade(s)  //{Math C}
+
+	s.InputGrade("English", "A")
+	s.ViewGrade() //{Math C} *고랭 함수호출은 복사
 }
