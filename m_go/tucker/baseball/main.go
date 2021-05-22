d896fa3f744fbe8d024b4ed1b041158715a0e178
jexists <jexists486@gmail.com>
2021-05-22 17:46:42 +0900

    baseball setting

--- m_go/tucker/baseball/main.go
+++ m_go/tucker/baseball/main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
+	//랜덤 시드값 지정 (안할경우 숫자가 계속 똑같음)
+	rand.Seed(time.Now().UnixNano())
+
 	// 무작위 숫자 3개 만들기
-	numbers := MakeNumebers()
+	numbers := MakeNumbers()
 
 	cnt := 0
 
 	for {
 		cnt++
 		// 사용자의 입력
-		inputNubers := InputNubers()
+		inputNumbers := InputNumbers()
 
 		// 결과 비교
-		result := CompareNumbers(numbers, inputNubers)
+		result := CompareNumbers(numbers, inputNumbers)
 
 		PrintResult(result)
 
@@ -26,4 +33,49 @@ func main() {
 
 	// 게임끝 결과
 	fmt.Printf("%d 번만에 맞췄습니다. \n", cnt)
+}
+
+func MakeNumbers() [3]int {
+
+	// 0~9 겹치지 않는 랜덤 숫자 3개 반환
+	var result [3]int
+	for i := 0; i < 3; i++ {
+		for {
+			n := rand.Intn(10)
+			duplicated := false
+			for j := 0; j < i; j++ {
+				if result[j] == n {
+					duplicated = true
+					break
+				}
+			}
+			if !duplicated {
+				result[i] = n
+				break
+			}
+		}
+	}
+	fmt.Println(result)
+	return result
+}
+
+func InputNumbers() [3]int {
+	// 사용자 0~9 겹치지 않는 숫자 3개 입력받아 반환
+	var result [3]int
+	return result
+}
+
+func CompareNumbers(numbers, inputNubers [3]int) bool {
+	//두개의 숫자 비교를 해서 결과 반환
+
+	return true
+}
+
+func PrintResult(result bool) {
+	fmt.Println(result)
+}
+
+func IsGameEnd(result bool) bool {
+
+	return result
 }
