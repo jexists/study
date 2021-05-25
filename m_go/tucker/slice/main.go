7dee4a863c8dffec9b530538d1746544994c723e
jexists <jexists486@gmail.com>
2021-05-25 21:54:26 +0900

    동적배열

---
+++ m_go/tucker/slice/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	// 동적배열 vs 정적배열
+	// 정적배열: fixed size array (배열의 길이가 변하지 않는)
+	// ex) [10]int
+
+	// 동적배열: 길이가 늘어난다. / 실제 배열을 포인트하고 있다.
+
+	// 메모리 할당
+
+	// 동적배열
+	// c++ STL vector
+	// python slice
+	// java Array List
+	// C# List
+	// go slice
+
+	//1. 빈배열 만드는 방법 var a []int
+	//2. 초기값 a := []int{1,2,3,4}
+	//3. make 사용 a:= make([]int,3)
+	//4. make 사용 with cap a:= make([]int, 0, 8)
+}
