dddf8cf67a0dc391f3b6796a645655bfc5b3b3de
jexists <jexists486@gmail.com>
2021-06-01 21:48:11 +0900

    RemoveFront

--- m_go/tucker/slice/slice.go
+++ m_go/tucker/slice/slice.go
@@ -24,14 +24,21 @@ func main() {
 	// 일부분을 가져오는 것 (같은 메모리)
 
 	for i := 0; i < 5; i++ {
-		var back int
-		a, back = RemoveBack(a)
-		fmt.Printf("%d, ", back) // 10, 9, 8, 7, 6,
+		// var back int
+		var front int
+		// a, back = RemoveBack(a)
+		a, front = RemoveFront(a)
+		// fmt.Printf("%d, ", back) // 10, 9, 8, 7, 6,
+		fmt.Printf("%d, ", front) // 1, 2, 3, 4, 5,
 	}
 	fmt.Println()
-	fmt.Println(a) // [1 2 3 4 5]
+	fmt.Println(a) // [1 2 3 4 5] //[6 7 8 9 10]
 }
 
 func RemoveBack(a []int) ([]int, int) {
 	return a[:len(a)-1], a[len(a)-1]
+}
+
+func RemoveFront(a []int) ([]int, int) {
+	return a[1:], a[0]
 }
