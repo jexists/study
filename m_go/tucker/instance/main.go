7ee6625626fdaefdc92148207bb20d0914555e02
jexists <jexists486@gmail.com>
2021-06-06 21:49:19 +0900

    instance

--- m_go/tucker/instance/main.go
+++ m_go/tucker/instance/main.go
@@ -12,14 +12,23 @@ func SetName(t Student, newName string) {
 	t.name = newName
 }
 
-//Function 형태 - 기능 단위, procedure절차
+//Function 형태 - 기능 단위, procedure절차 (Input), -> 기능 순서 중요
 func SetNameFix(t *Student, newName string) {
 	t.name = newName
 }
 
 //매소드 형태의 표현 - object단위
+//(OOP, 주체(object)/관계(relationship/주체(object)) -> object와 object의 관계 relationship 중요 (Entity-Relatioship)
 func (t *Student) SetNameFixM(newName string) {
 	t.name = newName
+}
+
+func (t *Student) SetAge(age int) {
+	t.age = age
+}
+
+func PrintStudent(u *Student) {
+	fmt.Println(u)
 }
 
 func main() {
@@ -48,7 +57,15 @@ func main() {
 	SetNameFix(&a, "bbb") //Reference Type
 	fmt.Println(a)        //{bbb 20 10}
 
-	a.SetName("bbb")
-	fmt.Println(a) //{bbb 20 10}
+	a.SetNameFixM("bbb") //a=> instance
+	fmt.Println(a)       //{bbb 20 10}
 
+	var e *Student
+	e = &Student{"aaa", 20, 10}
+
+	e.SetNameFixM("bbb")
+	e.SetAge(30)
+	fmt.Println(e) //&{bbb 30 10}
+
+	PrintStudent(e)
 }
