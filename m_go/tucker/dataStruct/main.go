b9d216f80b5f796285dd7c33718f6a3327821e87
jexists <jexists486@gmail.com>
2021-06-17 22:26:54 +0900

    package

---
+++ m_go/tucker/dataStruct/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"dataStruct"
+	"fmt"
+)
+
+func main() {
+	list := &dataStruct.LinkedList{}
+	list.AddNode(0)
+
+	for i := 1; i < 10; i++ {
+		list.AddNode(i)
+	}
+
+	list.PrintNodes()
+	// 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+
+	list.RemoveNode(list.Root.next)
+
+	list.PrintNodes()
+	// 0 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+
+	list.RemoveNode(list.Root)
+
+	list.PrintNodes()
+	// 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+
+	list.RemoveNode(list.Tail)
+
+	list.PrintNodes()
+	// 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8
+
+	fmt.Printf("Tail:%d\n", list.Tail.val)
+	// Tail:8
+
+	list.PrintReverse()
+	// 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2
+
+	a := []int{1, 2, 3, 4, 5}
+	a = append(a[0:2], a[3:]...)
+	fmt.Println(a) // [1 2 4 5]
+}
