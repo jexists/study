247527cbdd6f7daa169e121fb5b6ee7a90cde6f4
jexists <jexists486@gmail.com>
2021-06-21 22:52:06 +0900

    queue

--- m_go/tucker/stack/main.go
+++ m_go/tucker/stack/main.go
@@ -16,4 +16,16 @@ func main() {
 		last, stack = stack[len(stack)-1], stack[:len(stack)-1]
 		fmt.Println(last) // 5 4 3 2 1
 	}
+
+	queue := []int{}
+	for i := 1; i <= 5; i++ {
+		queue = append(queue, i)
+	}
+	fmt.Println(queue) //[1 2 3 4 5]
+
+	for len(queue) > 0 {
+		var front int
+		front, queue = queue[0], queue[1:]
+		fmt.Println(front) // 1 2 3 4 5
+	}
 }
