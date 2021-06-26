8482293e40bfc740a8081dbc562c6a693b6317a1
jexists <jexists486@gmail.com>
2021-06-26 13:34:03 +0900

    tree DFS - 재귀호출방식

--- m_go/tucker/package/dataStruct/tree.go
+++ m_go/tucker/package/dataStruct/tree.go
@@ -1,5 +1,7 @@
 package dataStruct
 
+import "fmt"
+
 type TreeNode struct {
 	Val    int
 	Childs []*TreeNode
@@ -9,10 +11,26 @@ type Tree struct {
 	Root *TreeNode
 }
 
+func (t *TreeNode) AddNode(val int) {
+	t.Childs = append(t.Childs, &TreeNode{Val: val})
+}
+
 func (t *Tree) AddNode(val int) {
 	if t.Root == nil {
 		t.Root = &TreeNode{Val: val}
 	} else {
 		t.Root.Childs = append(t.Root.Childs, &TreeNode{Val: val})
+	}
+}
+
+func (t *Tree) DFS1() {
+	DFS1(t.Root)
+}
+
+func DFS1(node *TreeNode) {
+	fmt.Printf("%d =>", node.Val)
+
+	for i := 0; i < len(node.Childs); i++ {
+		DFS1(node.Childs[i])
 	}
 }
