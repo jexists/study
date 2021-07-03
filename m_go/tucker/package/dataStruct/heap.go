b22f4c5bf795b7b4623fb512569a687ede3ede6e
jexists <jexists486@gmail.com>
2021-07-03 12:07:16 +0900

    back up

--- m_go/tucker/package/dataStruct/heap.go
+++ m_go/tucker/package/dataStruct/heap.go
@@ -28,3 +28,40 @@ func (h *Heap) Push(v int) {
 func (h *Heap) Print() {
 	fmt.Println(h.list)
 }
+
+func (h *Heap) Pop int {
+	if len(h.list) == 0 {
+		return 0
+	}
+
+	top := h.list[0]
+	last := h.list[len(h.list)-1]
+	h.list = h.list[:len(h.list)-1]
+
+	h.list[0] = last
+	idx := 0
+
+	for idx < len(h.list) {
+		leftIdx := idx*2+1
+		left := h.list[leftIdx]
+		if leftIdx >= len(h.list) {
+			break
+		}
+		rightIdx := idx*2+2
+		right := h.list[rightIdx]
+		if rightIdx >= len(h.list) {
+			break
+		}
+
+		if left > last && left>=right {
+			h.list[idx], h.list[leftIdx] = h.list[idx*2+1], h.list[idx]
+
+			idx = leftIdx
+		} else if right > last {
+			h.list[idx], h.list[rightIdx] = h.list[rightIdx], h.list[idx]
+			idx = rightIdx
+		} else {
+			break
+		}
+	}
+}
